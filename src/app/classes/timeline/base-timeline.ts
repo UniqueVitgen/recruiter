bf6991610250343ac1022529f7fa7d3a1e6aa826
YuriRusakovich <1970.gippo@gmail.com>
2018-12-08 17:27:33 +0300

    upd classes

---
+++ src/app/classes/timeline/base-timeline.ts
@@ -0,0 +1,3 @@
+export class BaseTimeline {
+  type: string;
+}
---
+++ src/app/classes/timeline/node-timeline.ts
@@ -0,0 +1,5 @@
+import {BaseTimeline} from './base-timeline';
+
+export class NodeTimeline extends BaseTimeline {
+  comment: string;
+}
