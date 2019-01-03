5d24fe095e18e79289b6bb56752b56de4d61fc5c
vitali <vitgen1997@mail.ru>
2019-01-03 21:00:14 +0300

    update candidate modal

--- src/app/components/modals/candidate/candidate-modal/candidate-modal.component.ts
+++ src/app/components/modals/candidate/candidate-modal/candidate-modal.component.ts
@@ -39,7 +39,7 @@ export class CandidateModalComponent implements OnInit {
         name: '',
         surname: '',
         candidateState: {
-          name: ''
+          name: 'Open'
         },
         contacts: [
           {
@@ -61,7 +61,7 @@ export class CandidateModalComponent implements OnInit {
         surname: ['', Validators.compose([Validators.required, Validators.pattern(RegexpConst.LATIN_NAME)])],
         state: [''],
         position: [''],
-        status: [''],
+        status: ['Open', Validators.compose([Validators.required])],
         skype: [''],
         phone: ['', Validators.compose([Validators.pattern(RegexpConst.BELLARUSSIAN_PHONE)])],
         email: ['', Validators.compose([Validators.pattern(RegexpConst.EMAIL)])]
