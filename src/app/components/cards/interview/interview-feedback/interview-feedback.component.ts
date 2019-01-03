d69579ec886fa979b44188240830e517928c1e55
vitali <vitgen1997@mail.ru>
2019-01-03 22:18:15 +0300

    update feedback interview: add validation

--- src/app/components/cards/interview/interview-feedback/interview-feedback.component.ts
+++ src/app/components/cards/interview/interview-feedback/interview-feedback.component.ts
@@ -3,6 +3,7 @@ import {Interview, InterviewExtended} from '../../../../classes/interview';
 import {DevFeedback} from '../../../../classes/dev-feedback';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {valid} from 'semver';
+import {TypeCheckingWorker} from '../../../../workers/type-checking/type-checking.worker';
 
 @Component({
   selector: 'app-interview-feedback',
@@ -16,14 +17,22 @@ export class InterviewFeedbackComponent implements OnInit, OnChanges {
   @Output('focusoutAnyInput') outputOnFocusoutAnyInput: EventEmitter<DevFeedback> = new EventEmitter();
   @Output('saveClick') outputSaveClick: EventEmitter<DevFeedback> = new EventEmitter();
   @Output('clickDelete') outputClickDelete: EventEmitter<DevFeedback> = new EventEmitter();
-  public feedbackForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  public editedFeedback: DevFeedback;
+  private isSaved: boolean = true;
+  constructor(private typeChekingWorker: TypeCheckingWorker) { }
 
   ngOnInit() {
   }
-  onFocusAnyInput() {
-    if (this.isEdit) {
-      this.outputOnFocusoutAnyInput.emit(this.feedback);
+  onFocusAnyInput(value: boolean = true) {
+    if (value) {
+      if (this.isEdit) {
+        this.isSaved = true;
+        this.outputOnFocusoutAnyInput.emit(this.feedback);
+      }
+    } else {
+      if (this.isEdit) {
+        this.isSaved = false;
+      }
     }
   }
   saveFeedback() {
@@ -34,6 +43,9 @@ export class InterviewFeedbackComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (this.isSaved) {
+      this.editedFeedback = this.typeChekingWorker.parseObject(this.feedback);
+    }
     // if (this.feedback) {
     //   this.feedbackForm = this.fb.group({
     //   });
--- src/app/components/items/timeline/dev-feedback-timeline-item/dev-feedback-timeline-item.component.ts
+++ src/app/components/items/timeline/dev-feedback-timeline-item/dev-feedback-timeline-item.component.ts
@@ -1,15 +1,16 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Feedback} from '../../../../classes/feedback';
 import {DateTimeWorker} from '../../../../workers/date-time/date-time.worker';
 import {TranslateWorker} from '../../../../workers/translate/translate.worker';
 import {DevFeedback} from '../../../../classes/dev-feedback';
+import {TypeCheckingWorker} from '../../../../workers/type-checking/type-checking.worker';
 
 @Component({
   selector: 'app-dev-feedback-timeline-item',
   templateUrl: './dev-feedback-timeline-item.component.html',
   styleUrls: ['./dev-feedback-timeline-item.component.scss']
 })
-export class DevFeedbackTimelineItemComponent implements OnInit {
+export class DevFeedbackTimelineItemComponent implements OnInit, OnChanges {
   @Input() note: DevFeedback;
   @Output() changeCandidate: EventEmitter<any> = new EventEmitter();
   @Output() deleteEvent: EventEmitter<any> = new EventEmitter();
@@ -17,7 +18,7 @@ export class DevFeedbackTimelineItemComponent implements OnInit {
   viewOfDate: string;
   viewOfInterview: string;
   private isSaved: boolean = true;
-  constructor(private dateTimeWorker: DateTimeWorker, private translateWorker: TranslateWorker) { }
+  constructor(private dateTimeWorker: DateTimeWorker, private translateWorker: TranslateWorker, private typeCheckingWorker: TypeCheckingWorker) { }
   ngOnInit() {
     this.editedNote = Object.assign({}, this.note);
     this.viewOfDate = this.dateTimeWorker.getDateWithTime(this.editedNote.createdAt);
@@ -32,12 +33,21 @@ export class DevFeedbackTimelineItemComponent implements OnInit {
   }
   onFocusoutAnyInput(value: boolean = true) {
     if (value) {
+      this.isSaved = true;
       console.log('exp', this.editedNote);
       this.changeCandidate.emit(this.editedNote);
+    } else {
+      this.isSaved = false;
     }
   }
   delete() {
     this.deleteEvent.emit(this.editedNote);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.isSaved) {
+      this.editedNote = this.typeCheckingWorker.parseObject(this.note);
+    }
   }
 
 }
