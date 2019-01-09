fce5602c6334c35d23a57969198aef8553e766d8
vitali <vitgen1997@mail.ru>
2019-01-09 13:42:33 +0300

    update search

--- src/app/components/inputs/search-input/search-input.component.ts
+++ src/app/components/inputs/search-input/search-input.component.ts
@@ -10,14 +10,24 @@ export class SearchInputComponent implements OnInit {
   @Input() autocompleteList: string[];
   @Output('onSearch') outputOnSearch: EventEmitter<string> = new EventEmitter();
   @Output('clickAdvancedSearch') outputClickAdvancedSearch: EventEmitter<boolean> = new EventEmitter();
+  value: string;
+  private filteredAutocompleteList: string[];
   public isTurnOnAdvancedSearch: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
-  search(value) {
+  search(value: string) {
     console.log(value);
+    this.value = value;
+    const searchValue = value.toLowerCase();
+    if (this.autocompleteList) {
+      this.filteredAutocompleteList = this.autocompleteList.filter((item) => {
+        const itemSearch = item.toLowerCase();
+        return itemSearch.indexOf(searchValue) > -1;
+      });
+    }
     this.outputOnSearch.emit(value);
   }
   clickAdvancedSearch() {
